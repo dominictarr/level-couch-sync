4402ca29d69d48a49796a8caf1eeaf10f3ad14e3
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-17 23:12:21 +1300

    update example

--- example.js
+++ example.js
@@ -1,16 +1,17 @@
 var levelCouchSync = require('./')
-var bucket = require('range-bucket')
+var pad = require('padded-semver').pad
 
 if(!module.parent) {
-  var b = bucket('npm')
-  levelCouchSync(require('levelup')(process.env.HOME + '/.level-npm'), {
-    db: 'http://isaacs.iriscouch.com/registry',
-    map: function (data, emit) {
+  var db = require('level-sublevel')(require('levelup')(process.env.HOME + '/.level-npm'))
+  var packageDb = db.sublevel('package')
+  var versionDb = db.sublevel('version')
+  levelCouchSync('http://isaacs.iriscouch.com/registry', db, 'registry-sync', 
+    function (data, emit) {
       var doc = data.doc
 
       //don't allow keys with ~
       if(/~/.test(data.id)) return
-      emit(b('package', data.id), JSON.stringify({
+      emit(data.id, JSON.stringify({
         name        : doc.name,
         description : doc.keywords,
         readme      : doc.readme,
@@ -18,28 +19,26 @@ if(!module.parent) {
         author      : doc.author,
         licenses    : doc.licenses,
         repository  : doc.repository
-      }))
+      }), packageDb)
 
       //versions
-      var vers = data.versions
+      var vers = doc.versions
       for(var version in vers) {
         var ver = vers[version]
-        emit(b('version', [data.id, version]), JSON.stringify({
+        emit(data.id + '\xFF' + pad(version), JSON.stringify({
           name: ver.name,
           version: ver.version,
           dependencies: ver.dependencies,
           devDependencies: ver.devDependencies,
           description: ver.description
-        }))
+        }), versionDb)
       }
-
-    },
-  })
-  .on('data', function (data) {
-    console.log(data.id)
-  })
-  .on('progress', function (ratio) {
-    console.log(Math.floor(ratio*10000)/100)
-  })
+    })
+    .on('data', function (data) {
+      console.log(data.id, data.doc.versions && Object.keys(data.doc.versions))
+    })
+    .on('progress', function (ratio) {
+      console.log(Math.floor(ratio*10000)/100)
+    })
 }
 
