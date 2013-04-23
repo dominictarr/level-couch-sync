8045e80816e5f2567a48e605bcfca06fecda01c5
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-04-23 18:58:26 +0200

    callback -> map function

--- example/npm-basic.js
+++ example/npm-basic.js
@@ -7,7 +7,7 @@ var os = require('os')
 // Create a level-sublevel instance to store the documents
 var db = sublevel(levelup(os.tmpdir() + '/level-npm-basic'))
 
-// The sync. Since no callback has been given, this will sync all documents
+// The sync. Since no map function has been given, this will sync all documents
 // and store them as (key, value) == (data.id, JSON.stringify(data.doc)
 levelCouchSync('http://isaacs.iriscouch.com/registry', db, 'registry-sync')
 
