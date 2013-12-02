ddaa976e74f8d0e346f3e6eab5c9669b55498c73
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-02 21:06:13 +0800

    also, have a progress bar

---
+++ progress.js
@@ -0,0 +1,37 @@
+var createBar = require('another-progress-bar')
+
+module.exports = function (db, name, tagline) {
+  var bar = createBar('syncing with ' + name + '...')
+  var counter =
+  db
+  .on('backoff', function (n,d) {
+    var disconnected = 'disconnected. retrying in:'
+    bar.label(disconnected + d/1000)
+    clearInterval(counter)
+    counter = setInterval(function () {
+      bar.label(disconnected + (d -= 1000) / 1000)
+      if(d <= 0)
+        clearInterval(counter)
+    }, 1000)
+  })
+  .on('ready', function () {
+    clearInterval(counter)
+    bar.label('...connecting to '+name+'...')
+  })
+  .on('max', function (m) {
+    bar.label('connected to '+name+'!!!', 500)
+  })
+  .on('fail', function (err) {
+    bar.label('could not connect: ' + err.message, 500)
+  })
+  .on('progress', function (ratio) {
+    bar.progress(Math.floor(ratio*10000)/100, 100)
+  })
+  .on('data', function (data) {
+    var s = tagline(data)
+    if(s)
+      bar.label(s)
+  })
+
+    return bar
+}
